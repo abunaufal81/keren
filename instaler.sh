e6075ee38d3520565aa0b3c5583162d2244fb848
Abu naufal <61857627+abunaufal81@users.noreply.github.com>
2020-03-06 21:14:00 +0700

    Add files via upload

---
+++ instaler.sh
@@ -0,0 +1,36 @@
+#!/system/xbin/Bash
+#Author InYouDfv47
+#Selamat belajar
+
+Clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+Cyan='\033[36;1m'
+red='\033[31;1m
+white='\033[37;1m
+
+echo $cyan.membuat Tools Instaler sendiri di termu"
+echo $green"Daftar Toolsnya=>
+echo $yellow"[1] DDOS Website Dengan LITEDDOS"
+echo $yellow"[2] DDOS Website Dengan Torshammer"
+echo $yellow"[3] DDOS Website Dengan Xerxes"
+echo $yellow"[4] DDOS website dengar hammer"
+echo 
+echo $green
+read -p "	[pilih nomer]
+	root@Dfv4='>"bro
+	
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $green"Installing.....
+apt update
+apt install git
+git clone https://github.com/abunaufal81/keren
+MV keren shome
+CD shome/keren
+read -p "Masukkan Target:" target
+python2 keren.py -T -T target
+fi
